c12dd1f915bc51a61df282cd6b0aecf029547ea8
STN\tyler.hattan <tyler.hattan@nebraska.gov>
2021-06-25 15:13:37 -0500

    first Trips push

---
+++ Trips/Data/Services/TripService.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace Trips.Data
+{
+    public class TripService : ITripService
+    {
+        public void AddTrip(Trip trip)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteTrip(int tripId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Trip> GetAllTrips()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Trip GetTripById(int tripId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateTrip(int tripId, Trip trip)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
